c6ef518be1e376e5f941accda13179aeac661349
mcstitzer <mcstitzer@ucdavis.edu>
2023-12-03 15:49:16 -0500

    Create make_gene_trees_scinet.slurm.sh

---
+++ gene_trees/make_gene_trees_scinet.slurm.sh
@@ -0,0 +1,62 @@
+#!/bin/bash -login
+#SBATCH -D /90daydata/buckler_lab_panand/michelle.stitzer/gene_tree_unalignedfa
+#SBATCH -o /90daydata/buckler_lab_panand/michelle.stitzer/slurm-log/buildtrees-stdout-%j.txt
+#SBATCH -e /90daydata/buckler_lab_panand/michelle.stitzer/slurm-log/buildtrees-stderr-%j.txt
+#SBATCH -J buildtrees
+set -e
+set -u
+
+module load raxml
+module load mafft
+module load samtools
+
+## running first with 4 cpu per tree, can fail due to too little memory (~12 gb?)
+## rerun again, after
+# find -size 0c -delete
+## which gets rid of *.withPvCDS.aln.fa (and .aln.fa if it exists)
+## then rerun with --mem=48000
+## can go up if needed
+
+## running all at max limit of 2 days on short partition
+
+#ls *0.fa > Pv_falist.txt
+SAMPLE_LIST=($(<Pv_falist.txt))
+i=${SAMPLE_LIST[${SLURM_ARRAY_TASK_ID}]}
+## slurm array limit on ceres
+## so for second batch , add 10000
+#i=${SAMPLE_LIST[$((SLURM_ARRAY_TASK_ID+10000))]}
+
+if [[ "$i" != *"aln"* ]]
+then
+gene="${i%%.*}"
+
+## add paspalum gene
+## not checking anymore because Sheng-Kai wants the Pasp genomic region in there too (which i added to r script)
+if grep -q "^>$gene" ${gene}.fa; then
+echo "already there"
+else
+samtools faidx ../Pv.CDS.fa ${gene}.1.v3.1 >> ${gene}.fa
+fi
+
+
+if [ ! -f ${gene}.withPvCDS.aln.fa ]
+then
+mafft --genafpair --maxiterate 1000 --adjustdirection ${gene}.fa > ${gene}.withPvCDS.aln.fa
+sed -i 's/()//g' ${gene}.withPvCDS.aln.fa
+sed -i 's/_R_//g' ${gene}.withPvCDS.aln.fa
+
+fi
+
+## remove CDS before doing the gene tree
+if [ ! -f ${gene}.aln.fa ]
+then
+sed  '/^Pavag/,+1d' ${gene}.withPvCDS.aln.fa  > ${gene}.aln.fa
+fi
+
+
+if [ ! -f RAxML_bestTree.${gene} ]
+then
+raxmlHPC-PTHREADS-AVX -T 4 -m GTRGAMMA -p 12345 -x 12345 -# 100 -f a -s ${gene}.aln.fa -n ${gene}
+
+fi
+fi
