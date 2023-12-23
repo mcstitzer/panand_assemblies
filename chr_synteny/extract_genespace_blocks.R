58ce5d7410df179d4bd05c1957540ac06ded5e94
mcstitzer <mcstitzer@ucdavis.edu>
2023-12-23 14:40:05 -0500

    Update extract_genespace_blocks.R

--- chr_synteny/extract_genespace_blocks.R
+++ chr_synteny/extract_genespace_blocks.R
@@ -1,4 +1,4 @@
 ## recording what i sent mohamed
 
 gsbt=tetraploid$plotData$sourceData$blocks
-write.table(gsbt, '~/transfer/tetraploid_genespace_blocks.txt', row.names=F, col.names=T, sep='\t')
+write.table(gsbt, '~/transfer/tetraploid_genespace_blocks.txt', row.names=F, col.names=T, sep='\t', quote=F)
