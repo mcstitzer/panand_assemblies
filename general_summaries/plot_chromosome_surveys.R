ded04926ce8ab4f6ba9841b8ae343370fdd95a5e
mcstitzer <mcstitzer@ucdavis.edu>
2024-02-12 18:34:28 -0500

    Update plot_chromosome_surveys.R

--- general_summaries/plot_chromosome_surveys.R
+++ general_summaries/plot_chromosome_surveys.R
@@ -31,3 +31,31 @@ ggplot(a[substr(a$queryChr,1,1)=='c',], aes(x=queryStart, y=queryChr, color=fact
 dev.off()
 
 
+
+## plot big contigs of each assembly
+all=read.table('../panand_sp_ploidy.txt')
+all=all[!all$V2 %in% c('pprate', 'tdactm', 'tzopol', 'osativ', 'bdista', 'agerjg', 'svirid', 'eophiu'),]
+
+clen=lapply(all$V2, function(x) {
+# a=read.table(paste0(x, '-Pv-', 2*all$V3[all$V2==x]), header=T)
+  a=read.table(paste0('genomes/', all$V1[all$V2==x], '.fasta.fai'), header=F)
+ a$genome=x
+ return(a)
+ })
+
+
+at=do.call(rbind, clen)
+
+at10=at[at$V2>10e6,]
+at10=at10 %>% arrange(-V2)
+
+
+pdf('~/transfer/panand_explore_chrs.pdf', 16,10)
+for(i in 1:nrow(all)){
+print(
+  ggplot(at10[at10$genome==all$V2[i],], aes(x=V2, y=reorder(V1, V2))) + geom_point() + ggtitle(all$V2[i]) + geom_vline(xintercept=c(0, 416000000))
+  )
+  }
+
+#ggplot(a[substr(a$queryChr,1,1)=='c',], aes(x=queryStart, y=queryChr, color=factor(genecount))) + geom_point(pch='|') + geom_segment(aes(y=queryChr, yend=queryChr, x=0, xend=maxChrLen), color='black') 
+dev.off()
