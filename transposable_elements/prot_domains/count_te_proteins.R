ed014977cee5bd2afb2e23b3d8b97631d1df5f74
mcstitzer <mcstitzer@ucdavis.edu>
2024-02-14 00:31:15 -0500

    Create count_te_proteins.R

---
+++ transposable_elements/prot_domains/count_te_proteins.R
@@ -0,0 +1,26 @@
+library(rtracklayer)
+library(stringr)
+library(plyranges)
+library(dplyr)
+
+all=read.table('../panand_sp_ploidy.txt')
+all=all[!all$V2 %in% c('pprate', 'tdactm', 'tzopol', 'osativ', 'bdista', 'agerjg', 'svirid', 'eophiu'),]
+
+yw=lapply(all$V2, function(x) {
+ a=import.gff(paste0(x, '.ywminiprot.gff'))
+ a=a[a$type=='mRNA',]
+  ## eventually deal with intersections (rank by score)
+# a=
+ a$genome=x
+ return(a)
+ })
+
+
+at=do.call(c, yw)
+at$name=str_split_fixed(at$n, ' ', 3)[,1]
+
+
+tegroup=read.table('yuan_wessler_2011.')
+at$sup=tegroup$sup[match(at$name, tegroup$fasta_name)]
+
+at %>% group_by(sup, genome) %>% dplyr::summarize(n=n()) %>% pivot_wider(names_from=sup, values_from=n)
