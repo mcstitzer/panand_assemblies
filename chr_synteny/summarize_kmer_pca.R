1cefbdfe333effdbdc8685e29bc6c4e79f94e6b7
mcstitzer <mcstitzer@ucdavis.edu>
2024-03-19 19:13:08 -0400

    Create summarize_kmer_pca.R

---
+++ chr_synteny/summarize_kmer_pca.R
@@ -0,0 +1,23 @@
+library(stringr)
+library(ggplot2)
+library(cowplot)
+theme_set(theme_cowplot())
+library(ggfortify)
+
+a=Sys.glob('*/*.kmer.mat')
+
+b=data.frame(genome=substr(a, 1,6), kmer=gsub('k','', unlist(str_extract_all(a, "k(\\d+)"))), q=gsub('q','',str_split_fixed(a, '_',3)[,2]))
+
+pdf('biplot_all_subphaser.pdf',10,10)
+
+for(i in 1:length(a)){
+mat=read.table(a[i], header=T)
+pca=prcomp(mat[,-1], rank=2)
+autoplot(pca, data = mat, label = TRUE, label.size = 3, loadings = TRUE, loadings.colour = 'blue',
+         loadings.label = TRUE, loadings.label.size = 3)
+pcprops=pca$sdev^2/sum(pca$sdev^2)
+b$PC1prop[i]=pcprops[1]
+b$PC2prop[i]=pcprops[2]
+}
+
+dev.off()
