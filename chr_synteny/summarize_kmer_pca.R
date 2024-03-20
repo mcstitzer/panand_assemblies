dfbf691ab58a8d60f61bb63a25f1c4ceb6485bd5
mcstitzer <mcstitzer@ucdavis.edu>
2024-03-20 19:25:41 -0400

    Update summarize_kmer_pca.R

--- chr_synteny/summarize_kmer_pca.R
+++ chr_synteny/summarize_kmer_pca.R
@@ -12,9 +12,12 @@ pdf('biplot_all_subphaser.pdf',10,10)
 
 for(i in 1:length(a)){
 mat=read.table(a[i], header=T)
-if(nrow(mat)>1){
-pca=prcomp(mat[,-1])
-print(autoplot(pca, data = mat, label = TRUE, label.size = 3, loadings = TRUE, loadings.colour = 'blue',
+mat2=t(mat[,-1])
+rownames(mat2)=colnames(mat)[-1]
+colnames(mat2)=mat$kmer
+if(ncol(mat2)>1){
+pca=prcomp(mat2)
+print(autoplot(pca, data = mat2, label = TRUE, label.size = 3, loadings = TRUE, loadings.colour = 'blue',
          loadings.label = TRUE, loadings.label.size = 3) + ggtitle(a[i]))
 pcprops=pca$sdev^2/sum(pca$sdev^2)
 b$PC1prop[i]=pcprops[1]
