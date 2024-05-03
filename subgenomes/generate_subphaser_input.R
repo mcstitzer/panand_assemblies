26b643e78a1eb7e05cd071508bc2fc987b51d071
mcstitzer <mcstitzer@ucdavis.edu>
2024-05-02 23:11:40 -0400

    add doubled ploidy version awd

--- subgenomes/generate_subphaser_input.R
+++ subgenomes/generate_subphaser_input.R
@@ -43,3 +43,43 @@ output=c(paste(out$first, out$second, out$third, sep='\t'), singletons)
 
 }
 }
+
+
+## now try doubled ploidy??
+for( i in 1:nrow(all)){
+if(file.exists(paste0('../',all$V2[i],'-Pv-', all$V3[i]*2))){
+a=read.table(paste0('../',all$V2[i],'-Pv-', all$V3[i]*2), header=T)
+
+b=a[a$gene!='interanchor',]
+
+
+
+## building on the assumption that most chromosomes are syntenic...
+## jsut get the biggest n syntenic scaffolds to assign kmers! then add restt of scaffolds back in
+## where n is subgenome count (ploidy, or 2*ploidy if allelic)
+
+  ## for now, not worrying about allelic assemblies and hoping chromosomes will be random length by subgenome (not all longest will be alleles or something)
+
+ploidy=all$V3[i]
+if(ploidy==2){
+  
+grouped=b %>% group_by(refChr, queryChr) %>% summarize(n=n()) %>% top_n(n=ploidy) %>% data.frame() %>% filter(grepl('Chr', refChr))
+out=grouped %>% group_by(refChr) %>% summarize(first=first(queryChr), second=nth(queryChr, 2), third=nth(queryChr,3), fourth=nth(queryChr,4))
+## get all contigs with synteny! 
+singletons=unique(b$queryChr)[! unique(b$queryChr) %in% grouped$queryChr]
+output=c(paste(out$first, out$second, out$third, out$fourth, sep='\t'), singletons)
+  write.table(output, paste0(all$V2[i],'_subphaserinput.awd.txt'), col.names=F, row.names=F, quote=F)
+}
+  if(ploidy==3){
+    grouped=b %>% group_by(refChr, queryChr) %>% summarize(n=n()) %>% top_n(n=ploidy) %>% data.frame() %>% filter(grepl('Chr', refChr))
+out=grouped %>% group_by(refChr) %>% summarize(first=first(queryChr), second=nth(queryChr, 2), third=nth(queryChr,3), fourth=nth(queryChr,4), fifth=nth(queryChr,5), sixth=nth(queryChr,6))
+## get all contigs with synteny! 
+singletons=unique(b$queryChr)[! unique(b$queryChr) %in% grouped$queryChr]
+output=c(paste(out$first, out$second, out$third, out$fourth, out$fifth, out$sixth, sep='\t'), singletons)
+    write.table(output, paste0(all$V2[i],'_subphaserinput.awd.txt'), col.names=F, row.names=F, quote=F)
+    }
+  
+
+}
+}
+}}
